30a1a0c439f0a90b6f2e394de1aa69cd7223d2ca
TheDanielV <126925561+TheDanielV@users.noreply.github.com>
2023-06-03 20:09:11 -0500

    a

--- SistemaVentasRestaurante/src/test/java/Modelo/PedidoTest.java
+++ SistemaVentasRestaurante/src/test/java/Modelo/PedidoTest.java
@@ -5,11 +5,5 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PedidoTest {
-    @Test
-    public void pedido_test(){
-        int a = 2;
-        int b = 2;
-        assertEquals(a, b);
-    }
 
 }
---
+++ SistemaVentasRestaurante/src/test/java/Modelo/ValidacionTest.java
@@ -0,0 +1,56 @@
+package Modelo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ValidacionTest {
+    @Test
+    public void give_cedula_when_validate_correct_then(){
+        Validacion validacion = new Validacion();
+        String numCedula = "0876";
+        boolean asignado = validacion.validarCedula(numCedula);
+        assertFalse(asignado);
+       // assertFalse(validacion.validarCedula("0192"));
+
+    }
+    @Test
+    public void give_cedula_when_validate_incorrect_then(){
+        Validacion validacion = new Validacion();
+        String numCedula = "1726004615";
+        boolean asignado = validacion.validarCedula(numCedula);
+        assertTrue(asignado);
+    }
+    @Test
+    public void validacionNombreIncorrecto(){
+        Validacion validacion = new Validacion();
+        String nombre = "Danie1";
+        boolean asignado = validacion.validarNombre(nombre);
+        assertFalse(asignado);
+    }
+    @Test
+    public void validacionNombreCorrecto(){
+        Validacion validacion = new Validacion();
+        String nombre = "Daniel";
+        boolean asignado = validacion.validarNombre(nombre);
+        assertTrue(asignado);
+
+    }
+    @Test
+    public void validacionApellidoIncorrecto(){
+        Validacion validacion = new Validacion();
+        String nombre = "Varg0as";
+        boolean asignado = validacion.validarNombre(nombre);
+        assertFalse(asignado);
+    }
+    @Test
+    public void validacionApellidoCorrecto(){
+        Validacion validacion = new Validacion();
+        String nombre = "Vargas";
+        boolean asignado = validacion.validarNombre(nombre);
+        assertTrue(asignado);
+
+    }
+
+
+}
