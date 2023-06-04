947cc9830d65a55d62c49c142774dbb7f9685d95
TheDanielV <126925561+TheDanielV@users.noreply.github.com>
2023-06-03 20:36:22 -0500

    Test pedido add

--- SistemaVentasRestaurante/src/test/java/Modelo/PedidoTest.java
+++ SistemaVentasRestaurante/src/test/java/Modelo/PedidoTest.java
@@ -5,5 +5,11 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PedidoTest {
+    @Test
+    public void given_an_product_(){
+        Pedido pedido = new Pedido();
+        pedido.agregarPedido(new Producto(001, "hamburguesa", 2.00, 2));
+        assertEquals(4.00, pedido.getSubtotal(), 0.2);
+    }
 
 }
