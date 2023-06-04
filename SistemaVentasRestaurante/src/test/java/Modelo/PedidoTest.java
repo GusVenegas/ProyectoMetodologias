e99630a8fee6f9d73f84957b950835b2ed9aaad6
TheDanielV <126925561+TheDanielV@users.noreply.github.com>
2023-06-03 19:23:41 -0500

    a

---
+++ SistemaVentasRestaurante/src/test/java/Modelo/PedidoTest.java
@@ -0,0 +1,15 @@
+package Modelo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PedidoTest {
+    @Test
+    public void pedido_test(){
+        int a = 2;
+        int b = 2;
+        assertEquals(a, b);
+    }
+
+}
