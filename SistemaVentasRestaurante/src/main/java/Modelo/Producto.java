a8fdeefb80a266d2f3860e1b5d5b1c9565fa3779
TheDanielV <126925561+TheDanielV@users.noreply.github.com>
2023-06-03 19:18:05 -0500

    commit pom.xml Ya se puede generar el test

--- SistemaVentasRestaurante/src/main/java/Modelo/Producto.java
+++ SistemaVentasRestaurante/src/main/java/Modelo/Producto.java
@@ -1,47 +1,51 @@
 package Modelo;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 
-@Builder
-@Getter
-@Setter
-@Entity
-@Table(name = "producto")
 public class Producto {
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Integer id;
-    
-    @Column(name = "nombre")
+
+    int id;
     String nombre;
-    
-    @Column(name = "precio")
     double precio;
-    
-    @Column(name = "cantidad")
-    Integer cantidad;
-    
-    private double descuentoProducto(double precio, int cantidad){
-    	int temp = cantidad;
-    	if(cantidad>5) {
-    		temp -= 1;
-    	}
-    	for(int i = 0 ; i==temp ; i++) {
-    		precio += precio * temp;
-    	}
-    	return precio;
+    int cantidad;
+
+
+    public Producto(int id, String nombre, double precio, int cantidad) {
+        this.nombre = nombre;
+        this.id = id;
+        this.precio = precio;
+        this.cantidad = cantidad;
     }
-    
-    public String consultarStockBajo() {
-    	return (cantidad<=10)? "Stock agotandose, quedan "  + this.cantidad + " unidades" : "Aún queda stock suficiente";    	
+
+
+    public void setId(int id) {
+        this.id = id;
     }
-    
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
 }
